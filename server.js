5d98a8b3095b34a6b46bcd1358e5ea3eaaaecb8c
MarLatte <86508134+marlatte@users.noreply.github.com>
2024-04-30 12:55:34 -0400

    Serve data from filePath

--- server.js
+++ server.js
@@ -10,24 +10,42 @@ const __dirname = path.dirname(__filename);
 
 const server = createServer(async (req, res) => {
   try {
-    let filePath;
-    switch (req.url) {
-      case '/': {
-        filePath = path.join(__dirname, 'public', 'index.html');
-        break;
-      }
-      case '/about': {
-        filePath = path.join(__dirname, 'public', 'about.html');
-        break;
-      }
-      case '/contact': {
-        filePath = path.join(__dirname, 'public', 'contact.html');
-        break;
+    if (req.method === 'GET') {
+      let filePath, fileNotFound;
+
+      switch (req.url) {
+        case '/': {
+          filePath = path.join(__dirname, 'public', 'index.html');
+          break;
+        }
+        case '/about': {
+          filePath = path.join(__dirname, 'public', 'about.html');
+          break;
+        }
+        case '/contact': {
+          filePath = path.join(__dirname, 'public', 'contact.html');
+          break;
+        }
+
+        default:
+          filePath = path.join(__dirname, 'public', '404.html');
+          fileNotFound = 404;
+          break;
       }
 
-      default:
-        filePath = path.join(__dirname, 'public', '404.html');
-        break;
+      const data = await fs.readFile(filePath);
+      res.writeHead(fileNotFound || 200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error('Method not allowed');
     }
-  } catch (error) {}
+  } catch (error) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Server error');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
